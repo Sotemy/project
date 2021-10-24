159dd049f5d78b9a8c95542a0d7fd10fff343635
moninger <moninger76131@gmail.com>
2021-10-24 15:30:19 +0200

    updated .gitgnore

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ db = SQLAlchemy(app)
 # Sample HTTP error handling
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html'), 404
+    return render_template('404.html', error=error), 404
 
 # Import a module / component using its blueprint handler variable (mod_auth)
 from app.mod_auth.controllers import mod_auth as auth_module
--- app/mod_auth/forms.py
+++ app/mod_auth/forms.py
@@ -1,18 +1,19 @@
 # Import Form and RecaptchaField (optional)
 from flask_wtf import Form # , RecaptchaField
-
-import email_validator
 # Import Form elements such as TextField and BooleanField (optional)
 from wtforms import TextField, PasswordField # BooleanField
 
 # Import Form validators
-from wtforms.validators import Required, Email, EqualTo
+from wtforms.validators import Required, Email, EqualTo, email_validator
 
 
 # Define the login form (WTForms)
 
 class LoginForm(Form):
-    email    = TextField('Email Address', [Email(),
-                Required(message='Forgot your email address?')])
-    password = PasswordField('Password', [
-                Required(message='Must provide a password. ;-)')])
+    email    = TextField('Email Address', [Email(), Required(message='Forgot your email address?')])
+    password = PasswordField('Password', [ Required(message='Must provide a password. ;-)')])
+
+class RegisterForm(Form):
+    email=TextField('Email Address', [Email(), Required(message='Forgot your email address?')])
+    password = PasswordField('Password', [ Required(message='Must provide a password. ;-)')])
+    password2 = PasswordField('Password again', [ Required(message='Must provide a password. ;-)')])
